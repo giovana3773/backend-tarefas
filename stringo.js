6614981ad505bb30071b0fcad0e8a8e5261857ac
giovana3773 <giovana.real@escola.pr.gov.br>
2026-03-10 08:46:19 -0300

    Create stringo.js

---
+++ stringo.js
@@ -0,0 +1,12 @@
+console.log("ENDEREÇO COMPLETO");
+
+let rua = "Rua das Flores"; 
+let numero = 9851;
+let cideda = "Curitiba";
+let cep = "80010-000";
+
+let endereçoCompleto = rua + ", " + numero + " - " + cidade + " - " + cep;
+
+console.log(endereçoCompleto);
+
+//Adicione informações adicionais 
