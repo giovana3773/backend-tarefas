032302a2de8d524e85deea82c704a8f8e3caa2aa
giovana3773 <giovana.real@escola.pr.gov.br>
2026-03-17 11:54:12 +0000

    versao2

--- hora.js
+++ hora.js
@@ -1,1 +1,19 @@
-//valores iniciais do dia
+// Valores iniciais do dia
+let valorPorHora = 50;
+let horasTrabalhadas = 8; // Expediente padrão
+
+console.log("--- Resumo do Expediente ---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
+
+horasTrabalhadas++;
+console.log(`Após hora extra (horasTrabalhadas++): ${horasTrabalhadas}h`);
+
+horasTrabalhadas--;
+console.log(`Após saída antecipada: ${horasTrabalhadas}h`);
+
+//Cálculo final
+let taxaSistema = 15;
+let pagamentoTotal = (horasTrabalhadas * valorPorHora) - taxaSistema;
+
+console.log("\n--- Fechamento ---");
+console.log('Total a receber por ${horasTrabalhadas}h: R$ ${pagamentoTotal}');
