c33add2bca1ced86577652d7061273f5a242bde5
giovana3773 <giovana.real@escola.pr.gov.br>
2026-03-24 13:54:03 +0000

    versao1

---
+++ atributo.js
@@ -0,0 +1,15 @@
+let numero = 2;
+numero = numero * 2;
+
+console.log(numero);
+
+let numeroN = 2;
+numeroN *= 2;
+console.log(numeroN);
+
+// Comparando números
+const idadeDandara = 26;
+const minhaIdade = 17;
+
+const saoIguais = idadeDandara == minhaIdade;
+console.log('As idades são iguais?' + saoIguais)
