931f060f2c4a1f4c56ea2dcfd0d2ab70cd8ac538
giovana3773 <giovana.real@escola.pr.gov.br>
2026-04-07 11:54:28 +0000

    versao2

---
+++ pontuacao.js
@@ -0,0 +1,13 @@
+//Sistema de Pontuação de um jogo
+const nomeJogador = "sandra";
+let faseAual = 3;
+let vidasRestantes = 2;
+
+let pontosPorMissao = [85, 90,60,100];
+
+//pontosPorMissao.length
+let pontuacaoTotal = 0;
+
+console.log(`Seja bem vinda: ${nomeJogador}!` );
+console.log(pontosPorMissao);
+
