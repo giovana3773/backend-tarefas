206d16fe0eb27493d74a3a0211b9c162b92ed201
giovana3773 <giovana.real@escola.pr.gov.br>
2026-04-07 11:50:30 +0000

    versao3

--- soma.js
+++ soma.js
@@ -5,5 +5,11 @@ function somar(numero1, numero2) {
 console.log(somar(5,2));
 
 function verificarPar(numero) {
-    if(numero % 2 === 0 ) 
-} 
+    if(numero % 2 === 0 ) {
+        return "o número é ímpar"
+    } else {
+
+    }
+}; 
+
+console.log(verificarPar(11));
