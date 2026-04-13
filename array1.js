33445738198315773a768de616c44acbd9914625
giovana3773 <giovana.real@escola.pr.gov.br>
2026-04-13 11:52:17 +0000

    v1

---
+++ array1.js
@@ -0,0 +1,14 @@
+//Lista de frutas - ARRAY
+
+const frutas = ["maçã","Banana", "uva", "Manga"];
+ //ÌNDICE FRUTAS   0       1       2        3
+
+ console.log(frutas[0]);
+ console.log(frutas[2]);
+ console.log(frutas.length);
+ frutas[1] = "morango";
+ frutas.push("Pêssego");
+ frutas.unshift("pêra");
+ console.log(frutas);
+ console.log(frutas.length);
+
