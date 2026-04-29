9b8201a10fe9969b5feddf032e49735b498e47f4
giovana3773 <giovana.real@escola.pr.gov.br>
2026-04-29 09:00:48 -0300

    atualização2

---
+++ objeto.js
@@ -0,0 +1,17 @@
+
+const bancoAutomotivo = {
+    material: "Couro Natural",
+    cor: "Caramelo",
+    tamanho: "Padrão",
+    concluido: false
+}
+
+console.log("Material escolhido:", bancoAutomotivo.material);
+
+bancoAutomotivo.concluido = true;
+console.log(bancoAutomotivo);
+
+const lojaCosmético = {
+    material: "Cosméticos"
+   
+}
